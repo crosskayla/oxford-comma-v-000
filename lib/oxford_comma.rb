525cacab5c3027e75b9f2d761c5b12cf029463ac
Kayla Cross <kac288@georgetown.edu>
2018-02-17 17:07:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,9 @@ def oxford_comma(array)
   array.each do |word|
     until word == array.last
       word << ", "
+    end
+    word << ", and "
+  end
 end
 
 test_array = ["fiddleheads","okra","kohlrabi"]
