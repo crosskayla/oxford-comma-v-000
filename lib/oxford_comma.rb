6562613de3d5df0f398ff347170614593226e8ce
Kayla Cross <kac288@georgetown.edu>
2018-02-17 17:15:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ def oxford_comma(array)
     return array[0...-1].join(", ") + ", and #{array[-1]}"
   elsif array.size == 2
     return array.join(" and ")
-  elseif array.size == 1 
+  elsif array.size ==1
+    return array[0]
   end
 end
